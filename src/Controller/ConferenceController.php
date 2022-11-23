7dd92dfaaef7936f50b4a0efe76643a26961885e
yvoste <yves.mitaine@nordnet.fr>
2022-11-23 18:40:04 +0100

    work in progress

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,31 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-    /**
-     * @Route("/hello/{name}", name="homepage")
-     */
-    public function index(Request $request, string $name = ''): Response
+
+    // public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+    // {
+    //     return new Response($twig->render(
+    //         'conference/index.html.twig',
+    //         ['conferences' => $conferenceRepository->findAll(),]
+    //     ));
+    // }
+
+    private $twig;
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager, Environment $twig)
     {
-        //dump($request);
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-            'vari' => $name
-        ]);
-        /*return new Response(
-            <<<EOF
-                <html>
-                    <body>
-                        <img src="images/under-construction.gif" />
-                    </body>
-                </html>
-            EOF
-        );*/
+        $this->manager = $manager;
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+
+        return new Response($this->twig->render(
+            'conference/index.html.twig',
+            ['conferences' => $this->manager->getRepository(Conference::class)->findAll(),]
+        ));
+    }
+
+
+    /**
+     * @route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, Conference $conference): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $this->manager->getRepository(Comment::class)->getCommentPaginator($conference, $offset);
+
+        return new Response($this->twig->render(
+            'conference/show.html.twig',
+            [
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            ],
+        ));
     }
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Comment;
+use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @extends ServiceEntityRepository<Comment>
@@ -16,6 +18,8 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE = 2;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
@@ -39,28 +43,40 @@ class CommentRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Comment[] Returns an array of Comment objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function getCommentpaginator(Conference $conference, int $offset): Paginator
+    {
+        $query = $this->createQueryBuilder('c')
+            ->andWhere('c.conference = :conference')
+            ->setParameter('conference', $conference)
+            ->orderBy('c.date_add', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery();
+        return new Paginator($query);
+    }
 
-//    public function findOneBySomeField($value): ?Comment
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Comment[] Returns an array of Comment objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Comment
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
